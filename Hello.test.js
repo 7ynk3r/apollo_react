762975c349af9b1fbdf25bde0a98f73c5e149081
7ynk3r <elmaildejuan@gmail.com>
2017-09-25 21:35:28 -0700

    smal refactor

--- Hello.test.js
+++ Hello.test.js
@@ -1,15 +1,14 @@
 import 'react-native';
 import React from 'react';
 import App from './App';
-
-const sleep = (time) => new Promise(sucess => setTimeout(sucess, time));
-
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+
+const sleep = (time) => new Promise(sucess => setTimeout(sucess, time));
 
 it('app renders correctly', async () => {
   const tree = renderer.create(<App />);
   expect(tree).toMatchSnapshot();
-  await sleep(2000)
+  await sleep(2000);
   expect(tree).toMatchSnapshot();
 });
